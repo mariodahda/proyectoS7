748eca300d2bd80b6735e9b72d7fa28d514042f9
mariodahda <mariodahda@hotmail.com>
2025-04-04 11:32:42 -0600

    agregar header

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.header("Análisis de datos de coches usados")  # encabezado de la aplicación
+st.subheader("Visualización de datos de coches usados")  # subtítulo de la aplicación
+
+
 car_data = pd.read_csv("vehicles_us.csv")  # leer los datos
 hist_button = st.button("Construir histograma")  # crear un botón
 
